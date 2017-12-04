1ace8e9771031eb9b691b12de21492493ed24cf2
mig35 <mig35@mig35.com>
2017-12-04 19:17:33 +0200

    [bugfix] remove navigator because it is not needed here

--- app/src/main/java/io/scal/ambi/ui/auth/recover/RecoveryActivity.kt
+++ app/src/main/java/io/scal/ambi/ui/auth/recover/RecoveryActivity.kt
@@ -1,6 +1,5 @@
 package io.scal.ambi.ui.auth.recover
 
-import android.app.Fragment
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -10,14 +9,11 @@ import io.scal.ambi.R
 import io.scal.ambi.databinding.ActivityRecoveryBinding
 import io.scal.ambi.extensions.binding.toObservable
 import io.scal.ambi.extensions.view.enableCascade
-import io.scal.ambi.navigation.NavigateTo
 import io.scal.ambi.presentation.auth.RecoveryStateModel
 import io.scal.ambi.presentation.auth.RecoveryViewModel
 import io.scal.ambi.ui.global.AlertDialogShower
 import io.scal.ambi.ui.global.BaseActivity
-import ru.terrakok.cicerone.Navigator
 import ru.terrakok.cicerone.Router
-import ru.terrakok.cicerone.android.AppNavigator
 import javax.inject.Inject
 import kotlin.reflect.KClass
 
@@ -53,24 +49,6 @@ class RecoveryActivity : BaseActivity<RecoveryViewModel, ActivityRecoveryBinding
                                                   { router.exit() })
                 }
                 .addTo(destroyDisposables)
-    }
-
-    override val navigator: Navigator by lazy {
-
-        object : AppNavigator(this, R.id.container) {
-
-            override fun createActivityIntent(screenKey: String, data: Any?): Intent? =
-                    when (screenKey) {
-                        NavigateTo.REGISTER        -> null // todo go ro register
-                        NavigateTo.FORGOT_PASSWORD -> null // todo make forgot password screen
-                        NavigateTo.HOME            -> null // todo go to home
-                        else                       -> null
-                    }
-
-            override fun createFragment(screenKey: String, data: Any?): Fragment? {
-                return null
-            }
-        }
     }
 
     companion object {
