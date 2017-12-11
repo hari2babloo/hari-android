c77c97a15d36da674d8f32762b1bead9f18e5b7d
mig35 <mig35@mig35.com>
2017-12-11 10:42:39 +0200

    [task] update audience icons

--- app/src/main/java/io/scal/ambi/entity/feed/Audience.kt
+++ app/src/main/java/io/scal/ambi/entity/feed/Audience.kt
@@ -5,11 +5,11 @@ import io.scal.ambi.R
 enum class Audience(val titleId: Int, val iconId: Int) {
 
     COLLEGE_UPDATE(R.string.news_feed_college_update, R.drawable.ic_audience_college_update),
-    STUDENTS(R.string.news_feed_students, R.drawable.ic_audience_college_update),
-    FACULTY(R.string.news_feed_faculty, R.drawable.ic_audience_college_update),
-    STAFF(R.string.news_feed_staff, R.drawable.ic_audience_college_update),
+    STUDENTS(R.string.news_feed_students, R.drawable.ic_audience_students),
+    FACULTY(R.string.news_feed_faculty, R.drawable.ic_audience_faculty),
+    STAFF(R.string.news_feed_staff, R.drawable.ic_audience_staff),
     GROUPS(R.string.news_feed_groups, R.drawable.ic_audience_groups),
     CLASSES(R.string.news_feed_classes, R.drawable.ic_audience_classes),
-    COMMUNITIES(R.string.news_feed_communities, R.drawable.ic_audience_college_update),
-    NEWS(R.string.news_feed_news, R.drawable.ic_audience_college_update)
+    COMMUNITIES(R.string.news_feed_communities, R.drawable.ic_audience_communities),
+    NEWS(R.string.news_feed_news, R.drawable.ic_audience_news)
 }
