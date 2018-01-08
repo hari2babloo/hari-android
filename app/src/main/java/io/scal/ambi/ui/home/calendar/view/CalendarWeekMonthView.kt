844ea43de66bc75f90e42659a277024455ab807d
mig35 <mig35@mig35.com>
2018-01-08 13:59:22 +0200

    [task] update calendar view for optimization

--- app/src/main/java/io/scal/ambi/ui/home/calendar/view/CalendarWeekMonthView.kt
+++ app/src/main/java/io/scal/ambi/ui/home/calendar/view/CalendarWeekMonthView.kt
@@ -1,40 +1,49 @@
 package io.scal.ambi.ui.home.calendar.view
 
 import android.content.Context
-import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
 import io.scal.ambi.R
 import io.scal.ambi.databinding.ItemCalendarDayWithNameBinding
 
 class CalendarWeekMonthView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
-    ConstraintLayout(context, attrs, defStyleAttr) {
+    LinearLayout(context, attrs, defStyleAttr) {
 
     private var bindings: List<ItemCalendarDayWithNameBinding>? = null
 
     init {
         LayoutInflater.from(context).inflate(R.layout.element_calendar_week_month, this, true)
+
+        orientation = VERTICAL
     }
 
     internal fun setupDays(uiCalendarGroupDays: UICalendarGroupDays) {
         if (null == bindings) {
-            bindings = (0 until childCount).map { getChildAt(it) }.map { ItemCalendarDayWithNameBinding.bind(it) }
+            bindings = (0 until childCount)
+                .map { getChildAt(it) }
+                .map { it as ViewGroup }
+                .flatMap { viewGroup -> (0 until viewGroup.childCount).map { viewGroup.getChildAt(it) } }
+                .map { ItemCalendarDayWithNameBinding.bind(it) }
         }
 
-        uiCalendarGroupDays.days
+        val days = uiCalendarGroupDays.days
+        if (days.size % 7 != 0) {
+            throw IllegalArgumentException("days should be a multiple of 7")
+        }
+
+        days
             .forEachIndexed { index, uiCalendarDayWithEvents ->
                 val itemBinding = bindings!![index]
                 itemBinding.event = uiCalendarDayWithEvents
                 itemBinding.weekNameVisibility = index < 7
-                itemBinding.root.visibility = View.VISIBLE
                 itemBinding.executePendingBindings()
             }
 
-        (uiCalendarGroupDays.days.size until bindings!!.size)
-            .forEach {
-                val itemBinding = bindings!![it]
-                itemBinding.root.visibility = View.GONE
-            }
+        val visibleCount = days.size / 7
+        (0 until visibleCount).forEach { getChildAt(it).visibility = View.VISIBLE }
+        (visibleCount until childCount).forEach { getChildAt(it).visibility = View.GONE }
     }
 }
